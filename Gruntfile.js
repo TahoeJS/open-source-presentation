42edcc20736da56589672e80e69fdad124fa6198
Jordan Papaleo <jordan@Jordans-MacBook-Pro.local>
2017-05-13 16:37:37 -0700

    blar plop

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,192 +1,184 @@
 /* global module:false */
 module.exports = function(grunt) {
-	var port = grunt.option('port') || 8000;
-	var root = grunt.option('root') || '.';
+  var port = grunt.option('port') || 8000
+  var root = grunt.option('root') || '.'
 
-	if (!Array.isArray(root)) root = [root];
+  if (!Array.isArray(root))
+    root = [root]
 
-	// Project configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		meta: {
-			banner:
-				'/*!\n' +
-				' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
-				' * http://lab.hakim.se/reveal-js\n' +
-				' * MIT licensed\n' +
-				' *\n' +
-				' * Copyright (C) 2017 Hakim El Hattab, http://hakim.se\n' +
-				' */'
-		},
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*!\n' + ' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' + ' * http://lab.hakim.se/reveal-js\n' + ' * MIT licensed\n' + ' *\n' + ' * Copyright (C) 2017 Hakim El Hattab, http://hakim.se\n' + ' */'
+    },
 
-		qunit: {
-			files: [ 'test/*.html' ]
-		},
+    qunit: {
+      files: ['test/*.html']
+    },
 
-		uglify: {
-			options: {
-				banner: '<%= meta.banner %>\n'
-			},
-			build: {
-				src: 'js/reveal.js',
-				dest: 'js/reveal.min.js'
-			}
-		},
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>\n'
+      },
+      build: {
+        src: 'js/reveal.js',
+        dest: 'js/reveal.min.js'
+      }
+    },
 
-		sass: {
-			core: {
-				files: {
-					'css/reveal.css': 'css/reveal.scss',
-				}
-			},
-			themes: {
-				files: [
-					{
-						expand: true,
-						cwd: 'css/theme/source',
-						src: ['*.sass', '*.scss'],
-						dest: 'css/theme',
-						ext: '.css'
-					}
-				]
-			}
-		},
+    sass: {
+      core: {
+        files: {
+          'css/reveal.css': 'css/reveal.scss'
+        }
+      },
+      themes: {
+        files: [
+          {
+            expand: true,
+            cwd: 'css/theme/source',
+            src: [
+              '*.sass', '*.scss'
+            ],
+            dest: 'css/theme',
+            ext: '.css'
+          }
+        ]
+      }
+    },
 
-		autoprefixer: {
-			dist: {
-				src: 'css/reveal.css'
-			}
-		},
+    autoprefixer: {
+      dist: {
+        src: 'css/reveal.css'
+      }
+    },
 
-		cssmin: {
-			compress: {
-				files: {
-					'css/reveal.min.css': [ 'css/reveal.css' ]
-				}
-			}
-		},
+    cssmin: {
+      compress: {
+        files: {
+          'css/reveal.min.css': ['css/reveal.css']
+        }
+      }
+    },
 
-		jshint: {
-			options: {
-				curly: false,
-				eqeqeq: true,
-				immed: true,
-				esnext: true,
-				latedef: true,
-				newcap: true,
-				noarg: true,
-				sub: true,
-				undef: true,
-				eqnull: true,
-				browser: true,
-				expr: true,
-				globals: {
-					head: false,
-					module: false,
-					console: false,
-					unescape: false,
-					define: false,
-					exports: false
-				}
-			},
-			files: [ 'Gruntfile.js', 'js/reveal.js' ]
-		},
+    jshint: {
+      options: {
+        curly: false,
+        eqeqeq: true,
+        immed: true,
+        esnext: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        eqnull: true,
+        browser: true,
+        expr: true,
+        globals: {
+          head: false,
+          module: false,
+          console: false,
+          unescape: false,
+          define: false,
+          exports: false
+        }
+      },
+      files: ['Gruntfile.js', 'js/reveal.js']
+    },
+    connect: {
+      server: {
+        options: {
+          port: port,
+          base: root,
+          livereload: true,
+          open: true
+        }
+      }
+    },
+    zip: {
+      'reveal-js-presentation.zip': [
+        'index.html',
+        'css/**',
+        'js/**',
+        'lib/**',
+        'images/**',
+        'plugin/**',
+        '**.md'
+      ]
+    },
 
-		connect: {
-			server: {
-				options: {
-					port: port,
-					base: root,
-					livereload: true,
-					open: true
-				}
-			},
+    watch: {
+      js: {
+        files: [
+          'Gruntfile.js', 'js/reveal.js'
+        ],
+        tasks: 'js'
+      },
+      theme: {
+        files: [
+          'css/theme/source/*.sass', 'css/theme/source/*.scss', 'css/theme/template/*.sass', 'css/theme/template/*.scss'
+        ],
+        tasks: 'css-themes'
+      },
+      css: {
+        files: ['css/reveal.scss'],
+        tasks: 'css-core'
+      },
+      html: {
+        files: root.map(path => path + '/*.html')
+      },
+      markdown: {
+        files: root.map(path => path + '/*.md')
+      },
+      options: {
+        livereload: true
+      }
+    },
+    retire: {
+      js: [
+        'js/reveal.js', 'lib/js/*.js', 'plugin/**/*.js'
+      ],
+      node: ['.'],
+      options: {}
+    }
 
-		},
+  })
 
-		zip: {
-			'reveal-js-presentation.zip': [
-				'index.html',
-				'css/**',
-				'js/**',
-				'lib/**',
-				'images/**',
-				'plugin/**',
-				'**.md'
-			]
-		},
+  // Dependencies
+  grunt.loadNpmTasks('grunt-contrib-qunit')
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-sass')
+  grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.loadNpmTasks('grunt-autoprefixer')
+  grunt.loadNpmTasks('grunt-zip')
+  grunt.loadNpmTasks('grunt-retire')
 
-		watch: {
-			js: {
-				files: [ 'Gruntfile.js', 'js/reveal.js' ],
-				tasks: 'js'
-			},
-			theme: {
-				files: [
-					'css/theme/source/*.sass',
-					'css/theme/source/*.scss',
-					'css/theme/template/*.sass',
-					'css/theme/template/*.scss'
-				],
-				tasks: 'css-themes'
-			},
-			css: {
-				files: [ 'css/reveal.scss' ],
-				tasks: 'css-core'
-			},
-			html: {
-				files: root.map(path => path + '/*.html')
-			},
-			markdown: {
-				files: root.map(path => path + '/*.md')
-			},
-			options: {
-				livereload: true
-			}
-		},
+  // Default task
+  grunt.registerTask('default', ['css', 'js'])
 
-		retire: {
-			js: ['js/reveal.js', 'lib/js/*.js', 'plugin/**/*.js'],
-			node: ['.'],
-			options: {}
-		}
+  // JS task
+  grunt.registerTask('js', ['jshint', 'uglify', 'qunit'])
 
-	});
+  // Theme CSS
+  grunt.registerTask('css-themes', ['sass:themes'])
 
-	// Dependencies
-	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-sass' );
-	grunt.loadNpmTasks( 'grunt-contrib-connect' );
-	grunt.loadNpmTasks( 'grunt-autoprefixer' );
-	grunt.loadNpmTasks( 'grunt-zip' );
-	grunt.loadNpmTasks( 'grunt-retire' );
+  // Core framework CSS
+  grunt.registerTask('css-core', ['sass:core', 'autoprefixer', 'cssmin'])
 
-	// Default task
-	grunt.registerTask( 'default', [ 'css', 'js' ] );
+  // All CSS
+  grunt.registerTask('css', ['sass', 'autoprefixer', 'cssmin'])
 
-	// JS task
-	grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
+  // Package presentation to archive
+  grunt.registerTask('package', ['default', 'zip'])
 
-	// Theme CSS
-	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
+  // Serve presentation locally
+  grunt.registerTask('serve', ['connect', 'watch'])
 
-	// Core framework CSS
-	grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
-
-	// All CSS
-	grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
-
-	// Package presentation to archive
-	grunt.registerTask( 'package', [ 'default', 'zip' ] );
-
-	// Serve presentation locally
-	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
-
-	// Run tests
-	grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
-
-};
+  // Run tests
+  grunt.registerTask('test', ['jshint', 'qunit'])
+}
